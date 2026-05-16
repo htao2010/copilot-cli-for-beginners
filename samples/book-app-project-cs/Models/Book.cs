afe745c100bcfdcb288b0d19c9e8e5c44bb98727
htao2010 <61603587+htao2010@users.noreply.github.com>
2026-05-16 19:24:03 -0400

    deleted js and cs sample projects

--- samples/book-app-project-cs/Models/Book.cs
+++
@@ -1,9 +0,0 @@
-namespace BookApp.Models;
-
-public class Book
-{
-    public string Title { get; set; } = string.Empty;
-    public string Author { get; set; } = string.Empty;
-    public int Year { get; set; }
-    public bool Read { get; set; }
-}
